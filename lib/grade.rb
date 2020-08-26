ae3ded5c66f61b4ec15ecc181865ffe9afc74dba
J Perrin <perrinjack96@gmail.com>
2020-08-26 17:40:31 +0100

    Green: 1 return when singular green grade inputted

---
+++ lib/grade.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+def grade(_string)
+  'Green: 1'
+end
--- spec/grade_spec.rb
+++ spec/grade_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require './lib/grade.rb'
 RSpec.describe '#grade' do
   it 'returns Green : 1 for Green input' do
     expect(grade('Green')).to eq 'Green: 1'
